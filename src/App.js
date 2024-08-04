ee7115bc6d837722613888f98d73f3f8b488b353
Roy Chen <roy97chen@gmail.com>
2024-08-04 20:40:59 +0300

    finish programs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from './Components/Hero/Hero';
 import './App.css';
+import Programs from './Components/Programs/Programs';
 
 const App = () => {
   return (
     <div className='App'>
       <Hero />
+      <Programs />
     </div>
   );
 };
